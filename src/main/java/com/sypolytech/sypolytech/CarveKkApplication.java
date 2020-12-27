2b50a63028464a451e652dd304bcae603d5146ea
KS <kyusang.dev@gmail.com>
2020-12-27 21:31:47 +0900

    Initial commit

---
+++ src/main/java/com/sypolytech/sypolytech/CarveKkApplication.java
@@ -0,0 +1,13 @@
+package com.sypolytech.sypolytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarveKkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarveKkApplication.class, args);
+    }
+
+}
