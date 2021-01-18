e09f3479009b298d85dd1c2e6969665048cb7eaf
leegyeongdeok <fty483212@gmail.com>
2021-01-19 01:40:42 +0900

    nameChange

--- src/main/java/com/sypolytech/sypolytech/Controller/ViewController.java
+++ src/main/java/com/sypolytech/sypolytech/Controller/ViewController.java
@@ -1,13 +1,13 @@
 package com.sypolytech.sypolytech.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
     //메인
-    @RequestMapping(value = "/home")
+    @GetMapping(value = "")
     public String Home() {
         return "index";
     }
