791bd2708bfc35600d82951db06a4ff25b175a49
KS <kyusang.dev@gmail.com>
2021-01-30 16:26:34 +0900

    import 추가

--- src/main/java/com/sypolytech/sypolytech/Controller/ViewController.java
+++ src/main/java/com/sypolytech/sypolytech/Controller/ViewController.java
@@ -1,7 +1,10 @@
 package com.sypolytech.sypolytech.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class ViewController {
