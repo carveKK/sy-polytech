8c432aa04422af687c072a5f0fad545579442530
leegyeongdeok <fty483212@gmail.com>
2021-03-17 08:53:24 +0900

    customer JS

---
+++ src/main/resources/static/js/fragments/categoryHeader/customerHeader.js
@@ -0,0 +1,15 @@
+function companyInfo(){
+    location.href = "/company/info";
+}
+
+function ceoIntro(){
+    location.href = "/company/ceo";
+}
+
+function companyOrigin(){
+    location.href = "/company/origin";
+}
+function map(){
+    location.href = "/company/route";
+}
+
