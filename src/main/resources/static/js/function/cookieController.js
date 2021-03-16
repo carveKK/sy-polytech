cf69deb12d241e21e41ae79899b19eb4feaa41e7
leegyeongdeok <fty483212@gmail.com>
2021-03-17 08:50:07 +0900

    function JS (cookie, parameter, scrollerOver)

--- src/main/resources/static/js/function/cookieController.js
+++ src/main/resources/static/js/function/cookieController.js
@@ -1,10 +1,10 @@
 function getCookie(name) {
-    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    let value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
     return value ? value[2] : null;
 }
 
-var delCookie = function (name) {
-    var date = new Date();
+function delCookie(name) {
+    let date = new Date();
     date.setDate(date.getDate() - 10);
 
     document.cookie = name + '=; expires=Thu, 01 Jan 2010 00:00:10 GMT;';
--- src/main/resources/static/js/function/scrollerOverPrevent.js
+++ src/main/resources/static/js/function/scrollerOverPrevent.js
@@ -1,5 +1,5 @@
 document.addEventListener('scroll', function () {
-    var currentScrollValue = document.documentElement.scrollTop;
+    let currentScrollValue = document.documentElement.scrollTop;
 
     if (currentScrollValue < 0)
         event.returnValue = false;
