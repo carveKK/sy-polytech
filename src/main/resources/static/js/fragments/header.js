60025830369cfa016ef4804f2ec2f990dab9ca8c
leegyeongdeok <fty483212@gmail.com>
2021-03-17 08:51:54 +0900

    update footer, header

--- src/main/resources/static/js/fragments/header.js
+++ src/main/resources/static/js/fragments/header.js
@@ -3,21 +3,68 @@ $(document).ready(function () {
     headerManager();
 });
 
-function headerManager() {
-    var $headerWrap = $('#headerWrap');
-    var $headerLogo = $('.companyLogo');
-    var pathName = location.pathname;
 
-    console.log(pathName);
+function headerManager() {
+    const $headerWrap = $('#headerWrap');
+    const $headerLogo = $('.companyLogo');
+    let pathName = location.pathname;
 
     if (pathName != "/") {
-        $headerWrap.css('color', 'black');
+        $headerWrap.css('position', 'absolute');
+   /*     $headerWrap.css('color', 'black');
         $headerWrap.css('background-color', 'white');
-        $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidth.png")');
+        $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidth.png")');*/
 
     } else {
+        $headerWrap.css('position', 'fixed');
         $headerWrap.css('color', 'white');
         $headerWrap.css('background-color', 'rgba(0,0,0,0)');
-        $headerLogo.css('background-image', 'url("/images/headerFooter/logoStringWhite.png")');
+        $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidthWhite.png")');
+
     }
 }
+
+function mainHeaderManager(index) {
+    const $headerWrap = $('#headerWrap');
+    const $headerLogo = $('.companyLogo');
+    console.log(index);
+
+    if (index > 1) {
+        $headerWrap.css('color', 'black');
+        $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidth.png")');
+    }else{
+        $headerWrap.css('color', 'white');
+        $headerWrap.css('background-color', 'rgba(0,0,0,0)');
+        $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidthWhite.png")');
+    }
+
+    // if(index == 2){
+    //     $headerWrap.css('display', 'none');
+    //     return;
+    // }else{
+    //     $headerWrap.css('display', 'block');
+    // }
+
+
+}
+
+function home() {
+    location.href = "/";
+}
+
+function ceoInfo() {
+    location.href = "/company/ceo";
+}
+
+function map() {
+    location.href = "/company/map";
+}
+
+function companyInfo() {
+    location.href = "/company/info";
+}
+
+function inquiry() {
+    location.href = "/customer/inquiry";
+}
+
