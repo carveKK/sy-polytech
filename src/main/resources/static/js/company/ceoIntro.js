ba42062d8725d69e8dfb08f609fac329cab2ae26
leegyeongdeok <fty483212@gmail.com>
2021-04-29 00:33:53 +0900

    company 페이지 수정

--- src/main/resources/static/js/company/ceoIntro.js
+++ src/main/resources/static/js/company/ceoIntro.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     $("#ceoIntro").addClass("menuOn");
+
+    $(".firstLeft").css("left", 10);
+    $(".firstRight").css("right", 10);
+
+    let currentScrollValue = document.documentElement.scrollTop;
+    console.log('currentScrollValue is ' + currentScrollValue);
+
+    document.addEventListener('scroll', function () {
+        currentScrollValue = document.documentElement.scrollTop;
+        console.log('currentScrollValue is ' + currentScrollValue);
+
+        if(currentScrollValue >= 120){
+            $('.secondContent').css("top", 0);
+        }
+    })
 });
