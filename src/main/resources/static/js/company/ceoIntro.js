a42a0742775ff7738ba446fe7f8e12f354843ff7
leegyeongdeok <fty483212@gmail.com>
2021-06-18 06:39:26 +0900

    company update(반응형)

--- src/main/resources/static/js/company/ceoIntro.js
+++ src/main/resources/static/js/company/ceoIntro.js
@@ -1,18 +1,36 @@
 $(document).ready(function () {
     $("#ceoIntro").addClass("menuOn");
 
-    $(".firstLeft").css("left", 10);
-    $(".firstRight").css("right", 10);
 
-    let currentScrollValue = document.documentElement.scrollTop;
-    console.log('currentScrollValue is ' + currentScrollValue);
+    var filter = "win16|win32|win64|mac|macintel";
+    var webType = "";
+    if (navigator.platform) {
+        if (0 > filter.indexOf(navigator.platform.toLowerCase())) {
+            $(".firstRight").css("right", 0);
+            $(".firstLeft").css("left", 0);
 
-    document.addEventListener('scroll', function () {
-        currentScrollValue = document.documentElement.scrollTop;
-        console.log('currentScrollValue is ' + currentScrollValue);
+            document.addEventListener('scroll', function () {
+                let currentScrollValue = document.documentElement.scrollTop;
+                console.log('currentScrollValue is ' + currentScrollValue);
 
-        if(currentScrollValue >= 120){
-            $('.secondContent').css("top", 0);
+                if (currentScrollValue >= 250) {
+                    $('.secondContent').css("top", 0);
+                }
+
+            });
+        } else {
+            $(".firstRight").css("right", 10);
+            $(".firstLeft").css("left", 10);
+
+            let currentScrollValue = document.documentElement.scrollTop;
+            document.addEventListener('scroll', function () {
+                currentScrollValue = document.documentElement.scrollTop;
+                console.log('currentScrollValue is ' + currentScrollValue);
+                if (currentScrollValue >= 120) {
+                    $('.secondContent').css("top", 0);
+                }
+
+            })
         }
-    })
+    }
 });
