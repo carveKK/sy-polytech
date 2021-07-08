b781039edabc49353d065a8f1d8fd02881a87a30
leegyeongdeok <fty483212@gmail.com>
2021-07-08 11:37:37 +0900

    연혁 다국어

--- src/main/resources/static/js/company/companyOrigin.js
+++ src/main/resources/static/js/company/companyOrigin.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $("#companyOrigin").addClass("menuOn");
+    let nowSearchData = location.search;
+    if (nowSearchData == "?lang=en") {
+        $('.originContent img').attr('src', '/images/company/companyOrigin/origin_en.png')
+    } else {
+        $('.originContent img').attr('src', '/images/company/companyOrigin/origin.png')
+    }
 });
